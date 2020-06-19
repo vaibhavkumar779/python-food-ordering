a304258a1be69900aa27de3fb3c740835618b5c6
VaibhavKumar <56683532+vaibhavkumar-779@users.noreply.github.com>
2020-06-19 14:35:51 +0530

    Add files via upload

---
+++ menu_item.py
@@ -0,0 +1,17 @@
+#this is the the parent class used as blueprint
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return self.name + ': $' + str(self.price)
+
+    def get_total_price(self, count):
+        total_price = self.price * count
+
+        if count >= 3:
+            total_price *= 0.9
+
+        return round(total_price)
+
